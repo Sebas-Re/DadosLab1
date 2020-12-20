1e43c3ed223759fdd235ca006a71c4146dbaded7
Squoshy <32151264+Squoshy-Boop@users.noreply.github.com>
2020-12-20 19:41:02 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctime>
+#include <math.h>
+#include <windows.h>
+using namespace std;
+#include "rlutil.h"
+#include "Proyecto_bunco1.0_funciones.h"
+
+using namespace std;
+
+int main(){
+
+   /** DibujarDados(1,5);
+    cout<<endl<<endl<<endl;*/
+
+    bool PartidasIniciadas=false;
+    bool SalirPrograma=false;
+    setlocale(LC_ALL, "spanish");
+    system ("color 57");
+    int op=0;
+    int c=0;
+    while(SalirPrograma!=true){
+    system ("color 57");
+    system("cls");
+    op=Menu(op,PartidasIniciadas);
+    system("cls");
+    if (op==0){
+        SalirPrograma=true;
+    }
+    else{
+        if(op>=1&&op<=2){
+            PartidasIniciadas=true;
+        }
+    }
+
+    Opciones(op, PartidasIniciadas,c);
+
+   }
+
+return 0;
+}
+
+
+
+
